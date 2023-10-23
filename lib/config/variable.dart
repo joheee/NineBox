37cce1b6216986b9550feec4d549939f0833dfaa
joheee <94522173+joheee@users.noreply.github.com>
2023-10-23 14:59:35 +0700

    finishing game layout

--- lib/config/variable.dart
+++ lib/config/variable.dart
@@ -4,4 +4,6 @@ class Variable {
   static Color primaryColor = const Color(0xffFFFFFF);
   static Color secondaryColor = const Color(0xff00A9FF);
   static EdgeInsetsGeometry defaultPadding = const EdgeInsets.all(20);
+  static Color deactiveColor = const Color(0xffd9d9d9);
+  static Color activeColor = const Color(0xffF2994A);
 }
