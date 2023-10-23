acc0d0e041db65580144dc7cc2dc2fb8b0ac0133
joheee <94522173+joheee@users.noreply.github.com>
2023-10-23 12:07:27 +0700

    update target setup page

--- lib/page/setup.dart
+++ lib/page/setup.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mati_murup/config/variable.dart';
 
 class SetupPage extends StatelessWidget {
   const SetupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Variable.primaryColor,
+        body: const SizedBox(),
+      ),
+    );
   }
 }
