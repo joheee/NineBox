08bfde80a7b15f672326ef2a02fa2e66bb4e3423
joheee <94522173+joheee@users.noreply.github.com>
2023-10-23 12:02:11 +0700

    initial setup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mati_murup/page/setup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SetupPage();
   }
 }
