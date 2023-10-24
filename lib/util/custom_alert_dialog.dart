f0f5f4e5c1193060ea1384bd0ad4840e10eb71eb
joheee <94522173+joheee@users.noreply.github.com>
2023-10-24 23:09:10 +0700

    game page recursive

--- lib/util/custom_alert_dialog.dart
+++ lib/util/custom_alert_dialog.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:mati_murup/config/variable.dart';
 
 class CustomAlertDialog{
-  static void showErrorDialog(BuildContext context, String errorMessage) {
+  static void showErrorDialog(BuildContext context, String errorMessage, String closeMessage) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return Dialog(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: Variable.defaultPadding,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
                 Text(errorMessage),
-                const SizedBox(height: 15),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Close'),
-                ),
+                const SizedBox(height: 10.0,),
+                ElevatedButton(
+                  onPressed: () => Navigator.pop(context), 
+                  child: Text(closeMessage)
+                )
               ],
             )
           )
